0d2c200b9823b346d292f16fa7cda7a7f591a737
Túlio Thauã Dutra <tuliothauadutra@uniplaclages.edu.br>
2025-09-09 14:04:00 -0300

    feat: script de tabuada básico

---
+++ TÉCNICAS DE PROGRAMAÇÃO/Práticas Laboratório/Tabuada/Main.kt
@@ -0,0 +1,16 @@
+//Túlio Thauã Dutra >>
+fun main() {
+    
+    //Creating the Entry
+    val number = 7
+    var resultado = 0
+    
+    println("Tabuada do $number: ")
+    
+    //Process
+    for(i in 0..10){
+        //Spittin on the screen
+   	    resultado = number * i
+        println("$number X $i = $resultado") 
+    }
+}
